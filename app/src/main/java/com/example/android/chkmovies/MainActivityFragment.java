e630e05223c5ace84a169c01e0f157c972c2fe1e
Himakiran <himakiran@gmail.com>
2016-11-28 01:41:23 +0530

    CHK-Movies App... Was able to display the image urls got from the fetch task by putting both FetchTask and ImageAdapter classes as sub classes of MainActivtyFragment.
    Also used the fetch.execute().get() to wait for fetch to get the results before passing to imageadpater.

--- app/src/main/java/com/example/android/chkmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/android/chkmovies/MainActivityFragment.java
@@ -9,7 +9,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,6 +37,7 @@ public class MainActivityFragment extends Fragment {
     // IMG is the imageadpater that gets executed in postexecute of fetchMovie.
     public ImageAdapter IMG;
 
+
     public MainActivityFragment() {
     }
 
@@ -42,16 +48,8 @@ public class MainActivityFragment extends Fragment {
 
         View rootView = inflater.inflate(fragment_main, container, false);
 
-        /*
-           Now we run the fetch task to fetch image urls
-         */
-
-        FetchMovie fetch = new FetchMovie(getContext());
 
 
-        //execute the async task
-
-        fetch.execute("popular");
 
         // The we use the rootview to get the gridview
 
@@ -187,11 +185,11 @@ public class MainActivityFragment extends Fragment {
             super.onPostExecute(strings);
             Log.v("CHK-FETCH", "CHK-postexec");
 
-            /*imageUrlArray = new String[strings.length];
+            imageUrlArray = new String[strings.length];
             for (int i = 0; i < strings.length; i++) {
                 imageUrlArray[i] = strings[i];
                 Log.v("CHK-ON-POST-EXEC", imageUrlArray[i]);
-            }*/
+            }
 
             return;
         }
@@ -253,6 +251,101 @@ public class MainActivityFragment extends Fragment {
         }
 
     }
+
+
+    public class ImageAdapter extends BaseAdapter {
+        // Keep all Images in array
+        private String[] mThumbIds = {"http://image.tmdb.org/t/p/w185/9HE9xiNMEFJnCzndlkWD7oPfAOx.jpg", "http://image.tmdb.org/t/p/w185/xfWac8MTYDxujaxgPVcRD9yZaul.jpg",
+                "http://image.tmdb.org/t/p/w185/4Iu5f2nv7huqvuYkmZvSPOtbFjs.jpg", "http://image.tmdb.org/t/p/w185/lFSSLTlFozwpaGlO31OoUeirBgQ.jpg",
+                "http://image.tmdb.org/t/p/w185/e1mjopzAS2KNsvpbpahQ1a6SkSn.jpg", "http://image.tmdb.org/t/p/w185/5N20rQURev5CNDcMjHVUZhpoCNC.jpg"};
+        private Context mContext;
+        private LayoutInflater inflater;
+
+
+        // Constructor
+        public ImageAdapter(Context c) {
+            mContext = c;
+            inflater = LayoutInflater.from(c);
+       /* mThumbIds = new String[imageArray.length];
+        for(int i=0; i < imageArray.length; i++){
+            mThumbIds[i] = imageArray[i];
+            Log.v("CHK-mThumbsId",mThumbIds[i]);
+        }*/
+        /*
+           Now we run the fetch task to fetch image urls
+         */
+
+            FetchMovie fetch = new FetchMovie(c);
+            try {
+                mThumbIds = fetch.execute("popular").get();
+            } catch (java.util.concurrent.ExecutionException | java.lang.InterruptedException k) {
+                Log.e("CHK-IMG-ADP-Fetch", "fetchtask", k);
+            }
+
+        }
+
+        /*
+        we r not using the below getCount() , getItem() and getItemId()
+         */
+        @Override
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+    /*
+     Reference : http://www.androidtrainee.com/picasso-image-loader-with-gridview-for-android/
+     */
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+
+            if (convertView == null) {
+                imageView = new ImageView(mContext);
+
+                //imageView.setLayoutParams(new GridView.LayoutParams(100, 100));
+            /*
+            The below code allows you to set the dimens in dimens.xml and then use it here
+            https://myskillset.wordpress.com/2013/07/05/android-setting-size-of-grid-view-items-according-to-screensize/
+             */
+                //imageView.setLayoutParams(new GridView.LayoutParams((int)mContext.getResources().getDimension(R.dimen.width), (int)mContext.getResources().getDimension(R.dimen.height)));
+            /*
+            The below code solves the layout problem to show the images in equalsized tiles..
+            http://stackoverflow.com/questions/23204755/how-to-set-image-size-in-grid-view
+             */
+                imageView.setLayoutParams(new RelativeLayout.LayoutParams((int) mContext.getResources().getDimension(R.dimen.width), (int) mContext.getResources().getDimension(R.dimen.height)));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(0, 0, 0, 0);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            Log.v("CHK-IMG-ADAPTER", "GET-VIEW");
+            Picasso
+                    .with(mContext)
+                    .load(mThumbIds[position])
+                    .fit()
+                    .into(imageView);
+
+
+            return imageView;
+        }
+    }
+
+
+
+
+
 
 }
 
