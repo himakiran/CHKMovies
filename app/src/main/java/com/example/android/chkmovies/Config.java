857c0c1ed70d4edb00deaea141de944ce1ca0b8b
chkumar <himakiran@users.noreply.github.com>
2016-12-02 16:49:46 +0530

    CHK-Movies App... Project cleaned up by running the clean utility in gradle. Added a readme file in the previous commit on github to include the url of api-keys file which has been deleted from Config.java

--- app/src/main/java/com/example/android/chkmovies/Config.java
+++ app/src/main/java/com/example/android/chkmovies/Config.java
@@ -5,8 +5,5 @@ package com.example.android.chkmovies;
  */
 
 public class Config {
-    // The movie db api key
-    public static final String movDBAPIKey = "c690562b8ea669d80e602902ea80a888";
-    // YouTube developer key
-    public static final String uTubeAPIKey = "AIzaSyCD1UqRhHNfDnYKl9RAZFjxne68xzP2RZA";
+
 }
